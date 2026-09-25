de871539974df118cdec8cfd6e82c8717aee865c
Nicolas THIBAUT <nicolas.thibaut@horticulture.red>
2026-09-25 15:54:23 +0200

    Include source path in file hash to improve integrity

--- openwebui_nas_tools.py
+++ openwebui_nas_tools.py
@@ -4,7 +4,7 @@ author: Nicolas THIBAUT
 git_url: https://github.com/uppersafe/
 description: Search on NAS for information and fetch specific file content.
 license: AGPL-3.0-only
-version: 1.3.2
+version: 1.3.3
 required_open_webui_version: 0.10.2
 requirements: requests, paramiko, smbprotocol
 """
@@ -910,7 +910,7 @@ class Tools:
     ) -> tuple:
         async with get_async_db_context() as db:
             # Search for file in cache
-            file_hash = blake2b(content).hexdigest()
+            file_hash = blake2b(source.encode() + b"\0" + content).hexdigest()
             file_id, file_collection = await self._get_cache_file(
                 file_hash,
                 user=user,
