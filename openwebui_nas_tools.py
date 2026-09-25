1de310aa0625fed65918b0a10283b352df8266a1
Nicolas THIBAUT <nicolas.thibaut@horticulture.red>
2026-09-25 16:25:22 +0200

    Retrieve source from file metadata and fallback to collection metadata

--- openwebui_nas_tools.py
+++ openwebui_nas_tools.py
@@ -4,7 +4,7 @@ author: Nicolas THIBAUT
 git_url: https://github.com/uppersafe/
 description: Search on NAS for information and fetch specific file content.
 license: AGPL-3.0-only
-version: 1.3.3
+version: 1.3.4
 required_open_webui_version: 0.10.2
 requirements: requests, paramiko, smbprotocol
 """
@@ -1163,7 +1163,8 @@ class Tools:
         ):
             for distance, metadata, document in zip(distances, metadatas, documents):
                 file_id = metadata.get("file_id")
-                source = metadata.get("source")
+                file_metadata = await Files.get_file_metadata_by_id(file_id)
+                source = file_metadata.meta.get("source") or metadata.get("source")
                 source_hash = blake2b(source.encode()).hexdigest()
                 # Add new source to results or update existing source with new snippets
                 snippets = results.get(source_hash, {}).get("snippets", [])
